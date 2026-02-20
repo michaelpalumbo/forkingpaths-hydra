8abadc6de9c3df438fc90aa0663d2a24b0c05466
saadaslamali <149980567+saadaslamali@users.noreply.github.com>
2026-02-20 17:28:45 -0500

    Created a bridge that talks to Michael's computer

--- wsbridge.js
+++ wsbridge.js
@@ -1,13 +1,28 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
+const wsclient = new WebSocket('ws://172.20.10.5:8080/ws');
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
+    if(wsclient){
+      wsclient.send(data);
+    }
   });
 
-  ws.send('something');
+  // ws.send('something');
+});
+
+
+wsclient.on('error', console.error);
+
+wsclient.on('open', function open() {
+  // wsclient.send('something');
+});
+
+wsclient.on('message', function message(data) {
+  console.log('received: %s', data);
 });
